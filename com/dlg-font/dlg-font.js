25893fb2a10924c1b849f4044422dfd56ab28479
unknown <2697111697@qq.com>
2020-04-21 17:15:03 +0800

    message

--- com/dlg-font/dlg-font.js
+++
@@ -1,8 +0,0 @@
-function DlgFont (editor) {
-  this.show=function () {
-    this.$notepad_dlg_font.addClass('show');
-  };
-  
-
- 
-}
--- com/editor/editor.js
+++
@@ -1,8 +0,0 @@
-function Editor () {
-    this.$editor = $('<div class="notepad-editor"></div>');
-    this.$textarea = $('<textarea spellcheck="false" auto-size="none" wrap="off"></textarea>');
-    this.create=function () {
-      this.$editor.append(this.$textarea);
-      return this.$editor;
-    };
-}
